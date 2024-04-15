83a71f60363549714abb5e377b640c73e28aac78
James Rose <james.rose1103@protonmail.com>
2024-04-15 15:21:58 -0400

    Adding files

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,38 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("{}", name)
+}
+#[tauri::command]
+fn srchange(name: &str) -> String {
+    format!("{}", name)
+ //   println!(name)
+}
+#[tauri::command]
+fn cmdmake(writing: String){
+    println!("mainrs")
+}
+#[tauri::command]
+fn log(logs: &str){
+    println!("{}",logs)
+}
+#[tauri::command]
+fn gener(name: &str) -> String{
+    format!("{}", name)
+}
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        //.invoke_handler(tauri::generate_handler![srchange])
+        //.invoke_handler(tauri::generate_handler![cmdmake])
+        .invoke_handler(tauri::generate_handler![log])
+        .invoke_handler(tauri::generate_handler![gener])
+        .run(tauri::generate_context!())
+        //.invoke_handler(tauri::generate_handler![log])
+        //.run(tauri::generate_context!())
+        .expect("error while running tauri application");
+        println!("Hi")
+}
