99a4901f5f71f48509fce14b584138af4901935f
James Rose <james.rose1103@protonmail.com>
2024-04-22 10:21:38 -0400

    v1.0.4 - Reducing functions, changing conditional rendering variables, Changing menu look.

--- src/App.jsx
+++ src/App.jsx
@@ -4,79 +4,33 @@ import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("James");
-  const [name, setName] = useState("");
   const [furl, setfurl] = useState("Books/The_Temptation_Of_Saint_Anthony.html");
-  const [frameshow, setFrameshow] = useState(true);
   const [menshow, setMenshow] = useState(false);
-  const [bookshow, setBookshow] = useState(false);
-  const [bibleshow, setBibleshow] = useState(false);
-  //document.addEventListener('DOMContentLoaded', () => {
   // This will wait for the window to load, but you could
   // run this function on whatever trigger you want
  // 	invoke('close_splashscreen')
 //})
-  invoke('close_splacescreen');
-  async function greet() {
+ // invoke('close_splacescreen');
+//  async function greet() {
   	//inputter.currentTarget.value
   	//() => setfurl("menu.html")
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     
-    setfurl(await invoke("greet", { name }));
-  }
-  async function closesplash() {
-    invoke('close_splashscreen')
+   // setfurl(await invoke("greet", { name }));
+ // }
+  
+  
+  /*async function closesplash() {
+    invoke('ose_splashscreen')
   }
   async function opensplash() {
     invoke('open_splashscreen')
   }
-  async function frameshower() {
-    if (setMenshow == true){
-    	setFrameshow((frameshow))
-    }
-    else {
-        setFrameshow(true)
-    }
-  }
+  */
   async function togglemenu() {
     setMenshow((menshow) => !menshow)
-    setFrameshow((frameshow) => !frameshow)
-    if (bookshow == true) {
-    	setBookshow((bookshow) => !bookshow)
-    }
-    if (bibleshow == true) {
-      setBibleshow((bibleshow) => !bibleshow)
-    }
     }
     //setBookshow((bookshow) => false)
-  async function togglebooks() {
-    if (bibleshow == true) {
-      setBibleshow((bibleshow) => !bibleshow)
-    }
-    setBookshow((bookshow) => !bookshow)
-  }
-  async function togglebibles() {
-    if (bookshow == true) {
-      setBookshow((bookshow) => !bookshow)
-    }
-    setBibleshow((bibleshow) => !bibleshow)
-  }
-  async function toggleframe() {
-    //if (menshow == true) {
-    	//setFrameshow((frameshow => false))
-    //}
-    //else 
-    setFrameshow((frameshow) => !frameshow)
-  }
-  async function compress(url) {
-    setBookshow((bookshow) => false)
-    setMenshow((menshow) => false)
-    setfurl((furl) => url)
-    //setfurl(await invoke("greet", url));
-    //setfurl((furl) => "The_Confession_Of_Saint_Augustine/index.html")
-    //setfurl((furl) => url)
-    //() => setfurl(url)
-  }
  // async function setter(url) {
    // setfurl(await invoke("srchange", { url }));
     //setfurl((furl) => "The_Confession_Of_Saint_Augustine/index.html")
@@ -92,23 +46,20 @@ function App() {
     	<button class="menu" onClick={togglemenu}>
     		<h2>Menu</h2>
     	</button>
-
-    	{menshow && <ul><li><button class="menu" onClick={togglebooks}><h3>Books</h3></button></li><li><button class="menu" onClick={togglebibles}><h3>Bibles</h3></button></li></ul>}
-    	{bookshow && <h2 class="mentop">Books</h2>}
-    	{bibleshow && <h2 class="mentop">Bibles</h2>}
-    	<ul>
-    	{bookshow && <li><button class="menu" onClick={() => {setfurl("Books/Catechism/_INDEX.HTM"); togglemenu()}}><h3>The Catechism Of The Catholic Church</h3></button></li>}
-    	{bookshow && <li><button class="menu" onClick={() => {setfurl("Books/Compendium.html"); togglemenu()}}><h3>Compendium Of The Social Doctrine Of The Church</h3></button></li>}
-    	{bookshow && <li><button class="menu" onClick={() => {setfurl("Books/The_Temptation_Of_Saint_Anthony.html"); togglemenu()}}><h3>The Temptation Of Saint Anthony</h3></button></li>}
-    	{bookshow && <li><button class="menu" onClick={() => {setfurl("Books/The_Confession_Of_Saint_Augustine.html"); togglemenu()}}><h3>The Confession Of Saint Augustine</h3></button></li>}
-    	{bookshow && <li><button class="menu" onClick={() => {setfurl("Books/City_Of_God_Volume_1.html"); togglemenu()}}><h3>City Of God Volume 1</h3></button></li>}
-    	{bookshow && <li><button class="menu" onClick={() => {setfurl("Books/City_Of_God_Volume_2.html"); togglemenu()}}><h3>City Of God Volume 2</h3></button></li>}
-    	{bibleshow && <li><button class="menu" onClick={() => {setfurl("Bibles/NIV.html"); togglemenu()}}><h3>New International Version</h3></button></li>}
-    	{bibleshow && <li><button class="menu" onClick={() => {setfurl("Bibles/King_James.html"); togglemenu()}}><h3>The King James Bible</h3></button></li>}
-    	{bibleshow && <li><button class="menu" onClick={() => {setfurl("/Bibles/American/_INDEX.HTM"); togglemenu()}}><h3>The New American Bible</h3></button></li>}
-    	</ul>
-</header>
-      {frameshow && <iframe id="viewarea" src={furl} loading="lazy" />}
+    	</header>
+    	<table>
+    	<tr id="titles">
+    	{menshow && <td class="mentop"><h2>Books</h2></td>}
+    	{menshow && <td class="mentop"><h2 >Bibles</h2></td>}
+    	</tr>
+    	{menshow && <tr><td class="books"><button class="menu" onClick={() => {setfurl("Books/Catechism/_INDEX.HTM"); togglemenu()}}><h3>The Catechism Of The Catholic Church</h3></button></td><td class="bibles"><button class="menu" onClick={() => {setfurl("Bibles/NIV.html"); togglemenu()}}><h3>New International Version</h3></button></td></tr>}
+    	{menshow && <tr><td class="books"><button class="menu" onClick={() => {setfurl("Books/Compendium.html"); togglemenu()}}><h3>Compendium Of The Social Doctrine Of The Church</h3></button></td><td class="bibles"><button class="menu" onClick={() => {setfurl("Bibles/King_James.html"); togglemenu()}}><h3>The King James Bible</h3></button></td></tr>}
+    	{menshow && <tr><td class="books"><button class="menu" onClick={() => {setfurl("Books/The_Temptation_Of_Saint_Anthony.html"); togglemenu()}}><h3>The Temptation Of Saint Anthony</h3></button></td> <td class="bibles"><button class="menu" onClick={() => {setfurl("/Bibles/American/_INDEX.HTM"); togglemenu()}}><h3>The New American Bible</h3></button></td></tr>}
+    	{menshow && <tr><td class="books"><button class="menu" onClick={() => {setfurl("Books/The_Confession_Of_Saint_Augustine.html"); togglemenu()}}><h3>The Confession Of Saint Augustine</h3></button></td></tr>}
+    	{menshow && <tr><td class="books"><button class="menu" onClick={() => {setfurl("Books/City_Of_God_Volume_1.html"); togglemenu()}}><h3>City Of God Volume 1</h3></button></td></tr>}
+    	{menshow && <tr><td class="books"><button class="menu" onClick={() => {setfurl("Books/City_Of_God_Volume_2.html"); togglemenu()}}><h3>City Of God Volume 2</h3></button></td></tr>}
+    	</table>
+      {!menshow && <iframe id="viewarea" src={furl} loading="lazy" />}
       </div>
   );
 }
