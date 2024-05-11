873b7693ac982a29804f19a060373547b398203e
RoseBlume <james.rose1103@protonmail.com>
2024-05-10 21:32:20 -0400

    v1.3.0

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,8 +3,6 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_window_state::Builder::new().build())
-        .plugin(tauri_plugin_notification::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
