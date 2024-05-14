05f22d822eb341f7e17b64ce09b8275b33abc264
RoseBlume <james.rose1103@protonmail.com>
2024-05-13 21:07:46 -0400

    v1.2.2

--- sign.sh
+++ sign.sh
@@ -4,7 +4,7 @@ cd /home/james/Documents/v2/Rosary
 echo "Setting up Environment Variables"
 export ANDROID_HOME=/home/james/Android/Sdk
 export NDK_HOME=/home/james/Android/Sdk/ndk/27.0.11718014
-export JAVA_HOME=/home/james/Documents/Studio/android-studio-2023.2.1.25-linux/android-studio/jbr
+export JAVA_HOME=/home/james/Documents/Studio/android-studio/jbr
 
 npm install
 npm update
