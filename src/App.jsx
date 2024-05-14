583f939bff7c8920136724aa84b3244c0354a1d9
RoseBlume <james.rose1103@protonmail.com>
2024-05-13 21:45:58 -0400

    v2.2.3

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [bookshow, setBookshow] = createSignal(false);
   const [bibleshow, setBibleshow] = createSignal(false);
   const [disclaimer, setDisclaimer] = createSignal(false);
+  const [license, setLicense] = createSignal(false);
   // If not we need to request it
   async function togglemenu() {
     setMenshow((menshow) => !menshow);
@@ -25,9 +26,10 @@ function App() {
     <ul>
     <Show when={menshow()}>
       <div id="topper"></div>
-      <li><button class="menu" onClick={() => {setBibleshow(false); setDisclaimer(false); setBookshow((bookshow) => !bookshow)}}><h3>Books</h3></button></li>
-      <li><button class="menu" onClick={() => {setBookshow(false); setDisclaimer(false); setBibleshow((bibleshow) => !bibleshow)}}><h3>Bibles</h3></button></li>
-      <li><button class="menu" onClick={() => {setBookshow(false); setBibleshow(false); setDisclaimer((disclaimer) => !disclaimer)}}><h3>Disclaimer</h3></button></li>
+      <li><button class="menu" onClick={() => {setBibleshow(false); setDisclaimer(false); setLicense(false); setBookshow((bookshow) => !bookshow)}}><h3>Books</h3></button></li>
+      <li><button class="menu" onClick={() => {setBookshow(false); setDisclaimer(false); setLicense(false); setBibleshow((bibleshow) => !bibleshow)}}><h3>Bibles</h3></button></li>
+      <li><button class="menu" onClick={() => {setBookshow(false); setBibleshow(false); setLicense(false); setDisclaimer((disclaimer) => !disclaimer)}}><h3>Disclaimer</h3></button></li>
+      <li><button class="menu" onClick={() => {setBookshow(false); setBibleshow(false); setDisclaimer(false); setLicense((license) => !license)}}><h3>License</h3></button></li>
     </Show>
     </ul>
     <ul>
@@ -58,6 +60,29 @@ function App() {
     <Show when={disclaimer()}>
     <h2 class="mentop">Disclaimer</h2>
     <li><p id="disclaimer">This application contains Project Gutenbergs Assets that have been modified to provide a better user experience. By continuing to use this app you agree that you have read and understood the previous statements.</p></li>
+    </Show>
+    <Show when={license()}>
+    <h2 class="mentop">License</h2>
+    <li>
+    <p class="license">MIT License</p>
+    <p class="license">Copyright (c) 2024 RoseBlume</p>
+    <p class="license">Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:</p>
+    <p class="license">The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.</p>
+    <p class="license">THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    </p>
+    </li>
     </Show>
     </ul>
     {!menshow() && <iframe id="viewarea" src={furl()} loading="eager" />}
