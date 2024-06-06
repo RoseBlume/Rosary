c064e484804308811d52a4818422451191a0e720
RoseBlume <james.rose1103@protonmail.com>
2024-06-06 14:33:04 -0400

    Fixing Workflows

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import { internalIpV4 } from "internal-ip";
+
+const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -13,6 +16,14 @@ export default defineConfig(async () => ({
   server: {
     port: 1420,
     strictPort: true,
+    host: mobile ? "0.0.0.0" : false,
+    hmr: mobile
+      ? {
+          protocol: "ws",
+          host: await internalIpV4(),
+          port: 1421,
+        }
+      : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
