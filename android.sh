3a7cf9f57a907b03227d26c0602ed68ccf5368f7
RoseBlume <james.rose1103@protonmail.com>
2024-06-10 23:47:23 -0400

    Removing target, gen, and .cargo

--- android.sh
+++ android.sh
@@ -19,9 +19,9 @@ echo -e "\n\nGenerating keypair"
 keytool -genkeypair -v -keystore rosary.keystore -alias rosary -keyalg RSA -keysize 2048 -validity 10000
 
 echo -e "\n\nRunning zipalign"
-/run/media/james/tauri/Android/build-tools/35.0.0-rc4/zipalign -f -v 4 src-tauri/gen/android/app/build/outputs/apk/universal/release/app-universal-release-unsigned.apk Rosary.apk
+/run/media/james/tauri/Android/build-tools/34.0.0/zipalign -f -v 4 src-tauri/gen/android/app/build/outputs/apk/universal/release/app-universal-release-unsigned.apk Rosary.apk
 
 echo -e "\n\nSigning APK"
-/run/media/james/tauri/Android/build-tools/35.0.0-rc4/apksigner sign --ks rosary.keystore --ks-key-alias rosary Rosary.apk
+/run/media/james/tauri/Android/build-tools/34.0.0/apksigner sign --ks rosary.keystore --ks-key-alias rosary Rosary.apk
 
 echo -e "\n\nBuilding linux bundles"
